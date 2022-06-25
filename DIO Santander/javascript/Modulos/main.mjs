f19076713750aa93d335d9bb87f4fda0d8dfdf87
Reverdan Almeida Sparinger <reverdan@gmail.com>
2022-06-24 23:27:59 -0300

    Módulos

---
+++ DIO Santander/javascript/Modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Rever", 52));
+console.log(mostraCidade("Rever", "Sorocaba"));
+console.log(mostraHobby("Rever", "Eletronica"));
